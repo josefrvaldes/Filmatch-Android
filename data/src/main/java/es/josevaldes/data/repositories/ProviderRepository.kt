4234d36fb040999ed436463f6c56ef1840beae59
Jose Valdés <josefrvaldes@gmail.com>
2024-11-19 22:20:08 +0100

    We are now getting tv providers as well

--- data/src/main/java/es/josevaldes/data/repositories/ProviderRepository.kt
+++ data/src/main/java/es/josevaldes/data/repositories/ProviderRepository.kt
@@ -5,6 +5,8 @@ import es.josevaldes.data.model.Provider
 import es.josevaldes.data.results.ApiError
 import es.josevaldes.data.results.ApiResult
 import es.josevaldes.data.services.ProviderService
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retryWhen
@@ -14,11 +16,42 @@ class ProviderRepository @Inject constructor(private val _providerService: Provi
     fun getMovieProviders(language: String, region: String): Flow<ApiResult<List<Provider>>> =
         flow {
             try {
-                val result = _providerService.getMovieProviders(language, region)
-                if (result is ApiResult.Success) {
-                    emit(ApiResult.Success(result.data.toProvidersList()))
-                } else {
-                    emit(result as ApiResult.Error)
+                coroutineScope {
+                    // let's make two requests in parallel
+                    val movieProvidersResult =
+                        async { _providerService.getMovieProviders(language, region) }
+                    val tvProvidersResult =
+                        async { _providerService.getTvProviders(language, region) }
+
+                    val movieProviders = movieProvidersResult.await()
+                    val tvProviders = tvProvidersResult.await()
+
+                    val movieProvidersList = if (movieProviders is ApiResult.Success) {
+                        movieProviders.data.toProvidersList()
+                    } else {
+                        emptyList()
+                    }
+
+                    val tvProvidersList = if (tvProviders is ApiResult.Success) {
+                        tvProviders.data.toProvidersList()
+                    } else {
+                        emptyList()
+                    }
+
+                    val mergedProviders = (movieProvidersList + tvProvidersList)
+                        .distinctBy { it.id }
+
+                    if (mergedProviders.isNotEmpty()) {
+                        emit(ApiResult.Success(mergedProviders))
+                    } else {
+                        emit(
+                            ApiResult.Error(
+                                (movieProviders as? ApiResult.Error)?.apiError
+                                    ?: (tvProviders as? ApiResult.Error)?.apiError
+                                    ?: ApiError.Unknown
+                            )
+                        )
+                    }
                 }
             } catch (e: Exception) {
                 emit(ApiResult.Error(ApiError.Unknown))
