e90b45b48f216ea64332a190380d0ddba58424fe
Jose Valdés <>
2025-01-27 21:32:02 +0100

    Now the style is similar to figma

--- app/src/main/java/es/josevaldes/filmatch/ui/screens/HomeScreen.kt
+++ app/src/main/java/es/josevaldes/filmatch/ui/screens/HomeScreen.kt
@@ -1,16 +1,15 @@
 package es.josevaldes.filmatch.ui.screens
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,8 +18,12 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -43,7 +46,9 @@ fun HomeScreen(mainNavController: NavController) {
 
 
     Scaffold(
-        modifier = Modifier.statusBarsPadding(),
+        modifier = Modifier
+            .statusBarsPadding()
+            .navigationBarsPadding(),
         bottomBar = {
             BottomNavBar(
                 onItemSelected = { index ->
@@ -101,31 +106,31 @@ fun BottomNavBar(
         BottomNavBarRoute(
             stringResource(R.string.bottom_bar_menu_discover_title),
             Route.SlideMovieRoute,
-            Icons.Default.Favorite
+            ImageVector.vectorResource(R.drawable.ic_discover)
         ),
         BottomNavBarRoute(
             stringResource(R.string.bottom_bar_menu_search_title),
             Route.SearchRoute,
-            Icons.Default.Search
+            ImageVector.vectorResource(R.drawable.ic_search)
         ),
         BottomNavBarRoute(
             stringResource(R.string.bottom_bar_menu_matches_title),
             Route.MatchesRoute,
-            Icons.Default.ThumbUp
+            ImageVector.vectorResource(R.drawable.ic_rooms)
         ),
         BottomNavBarRoute(
             stringResource(R.string.bottom_bar_menu_rooms_title),
             Route.RoomsRoute,
-            Icons.Default.Home
+            ImageVector.vectorResource(R.drawable.ic_rooms)
         ),
         BottomNavBarRoute(
             stringResource(R.string.bottom_bar_menu_profile_title),
             Route.ProfileRoute,
-            Icons.Default.Person
+            ImageVector.vectorResource(R.drawable.ic_profile)
         )
     )
 
-    NavigationBar {
+    NavigationBar(modifier = Modifier.height(96.dp)) {
         topLevelRoutes.forEachIndexed { index, currentItem ->
             NavigationBarItem(
                 selected = currentDestination?.hierarchy?.any {
@@ -135,10 +140,27 @@ fun BottomNavBar(
                 } == true,
                 onClick = { onItemSelected(index) },
                 icon = {
-                    Icon(imageVector = currentItem.icon, contentDescription = currentItem.name)
+                    Icon(
+                        modifier = Modifier
+                            .width(48.dp)
+                            .height(48.dp),
+                        imageVector = currentItem.icon,
+                        contentDescription = currentItem.name
+                    )
                 },
-                label = { Text(text = currentItem.name) }
+                label = { Text(text = currentItem.name) },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.Transparent,
+                    selectedIconColor = MaterialTheme.colorScheme.secondary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground
+                )
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun BottomNavBarPreview() {
+    BottomNavBar(selectedEntry = null, onItemSelected = {})
 }
